ef0c219e7df129a54b582749d64a06c3070750d1
OswaldoFalcon <owa11@hotmail.com>
2022-06-13 00:46:06 -0500

    Fix Css

--- lib/game_of_life_web/live/game_of_life_live.ex
+++ lib/game_of_life_web/live/game_of_life_live.ex
@@ -18,6 +18,7 @@ defmodule GameOfLifeWeb.GameOfLifeLive do
         board: "",
         size: 5,
         tref: {},
+        probability: 0 , 
         grid: %Conway.Grid{
           data:
             {{0, 1, 0, 0, 0}, {0, 0, 0, 0, 1}, {1, 0, 1, 0, 1}, {1, 1, 1, 1, 0}, {0, 1, 0, 1, 1}}
@@ -28,57 +29,45 @@ defmodule GameOfLifeWeb.GameOfLifeLive do
 
   def render(assigns) do
     ~L"""
-    <div>
-    Grid Dimension: <br>
-     50 x 50  <input type="checkbox"  phx-click="dimension"  phx-value-dimension="50"> <br>
-     20 x 20  <input type="checkbox"  phx-click="dimension"  phx-value-dimension="20"> <br>
-     10 x 10  <input type="checkbox"  phx-click="dimension"  phx-value-dimension="10"> <br>
-     5 x 5    <input type="checkbox"  phx-click="dimension"  phx-value-dimension="5"> <br>
-    </div>
-
-     <h2>
-       <%= @message %> <br>
-     </h2>
-
-     <div class="board-container">
-        <div class="board">
-          <%= for y <- 0..(Conway.Grid.size(@grid) - 1) do %>
-            <div class="row"> □
-              <%= for  x <- 1..(Conway.Grid.size(@grid)- 1) do %>
-                <div class="cell"> 
-                <%= case Conway.Grid.cell_status(@grid, x, y) do %>
-                  <% 0 -> %> □
-                  <% 1 -> %> ■
-                <% end%>
-                </div>
-              <% end %>
-            </div>
-          <% end %>
-        </div>
+    <div class= "container">
+      <div class = "grid_size" >
+        <div> Grid Dimension: </div> 
+        <div> 50 x 50 </div> <input type="checkbox"  phx-click="dimension"  phx-value-dimension="50"> <br>
+        <div> 20 x 20 </div> <input type="checkbox"  phx-click="dimension"  phx-value-dimension="20"> <br>
+        <div> 10 x 10 </div> <input type="checkbox"  phx-click="dimension"  phx-value-dimension="10"> <br>
+        <div> 10 x 10 </div> <input type="checkbox"  phx-click="dimension"  phx-value-dimension="5"> <br>
       </div>
-
-
-      <button phx-click="go">Go</button>  
-      <button phx-click="stop">Stop</button>  
-      <button phx-click="randomize">Randomize</button>  
-
+       <p>
+         <%= @message %> <br>
+       </p>
+       <div class="board-container">
+          <div class="board">
+            <%= for y <- 0..(Conway.Grid.size(@grid) - 1) do %>
+              <div class="row"> 
+                <%= for  x <- 0..(Conway.Grid.size(@grid)- 1) do %>
+                  <div class="cell"> 
+                  <%= case Conway.Grid.cell_status(@grid, x, y) do %>
+                    <% 0 -> %> <div class = "dead"> </div>
+                    <% 1 -> %> <div class = "alive" > </div>
+                  <% end%>
+                  </div>
+                <% end %>
+              </div>
+            <% end %>
+          </div>
+        </div>
+        <div class = "buttons">
+          <button phx-click="go"> Go </button>  
+          <button phx-click="stop"> Stop </button>  
+          <div class="slidecontainer">
+            <button phx-click="randomize"> Randomize </button>        
+            <input type="range" min="0" max="10" value="1" class="slider" id="myRange" >
+            the probability is <%= @probability %>
+            </div>  
+        </div>
+    </div >
     """
   end
-
-  # def handle_event("randomize", _, socket) do
-  #   message = "Yu start randomize"
-  #   grid = TerminalGame.playliveview(socket.assigns.grid,"on livewviw")
-  #   {
-  #     :noreply,
-  #     assign(
-  #       socket,
-  #       message: message,
-  #       grid: grid,
-  #     )
-  #   }
-  # end
-
-  # def 
 
   def handle_event("dimension", %{"dimension" => dimension} = data, socket) do
     IO.inspect(data)
@@ -142,12 +131,19 @@ defmodule GameOfLifeWeb.GameOfLifeLive do
     }
   end
 
+   def handle_event("probability",%{"probability" => probability}, socket) do
+     {
+       :noreply,
+       assign(
+         socket,
+         probability: probability
+       )
+     }
+   end
+  
   def handle_info(:tick, %{assigns: %{grid: grid}} = socket) do
-    # spawn(TerminalGame, :play, [])
     grid = TerminalGame.playliveview(grid, "Displaying with livewView")
-    # message = "#{self()}"
     message = "If yu want to stop press STOP button"
-    # {:noreply, socket |> assign(:grid, grid)}
     GameOfLifeWeb.Endpoint.broadcast_from(self(), @topic, "update_grid", %{grid: grid})
 
     {
